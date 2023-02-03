7d020794f2487c43b65743d1103b6ff32180d251
blobbybilb <blobbybilb@randomblob.studio>
2023-02-02 23:25:10 -0800

    update config

--- config/config.go
+++ config/config.go
@@ -4,8 +4,15 @@ const (
 	Address = "0.0.0.0"
 	Port    = 6010
 
-	StoreType = "file" // "file"
+	StoreType = "postgres" // "file" or "postgres"
 
 	// file store config
 	DataDir = "./data/"
+
+	// postgres store config
+	PostgresHost = "<ip, domain, localhost, etc.>"
+	PostgresPort = 5432
+	PostgresUser = "<username>"
+	PostgresPass = "<password>"
+	PostgresDB   = "<db_name>"
 )
